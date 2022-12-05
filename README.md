11fe706251a74f2d85a1b9458ad0813f9d6db8fb
maksimgorbunov <maksim.gorbunov@piano.io>
2022-12-05 12:27:46 +0400

    base implementation

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to use 
+1. Build ./gradlew jar
+2. Run **may require sudo**
+```shell
+java  --add-opens java.base/java.lang=ALL-UNNAMED --add-modules jdk.hotspot.agent --add-exports jdk.hotspot.agent/sun.jvm.hotspot=ALL-UNNAMED --add-exports jdk.hotspot.agent/sun.jvm.hotspot.tools=ALL-UNNAMED --add-exports jdk.hotspot.agent/sun.jvm.hotspot.runtime=ALL-UNNAMED --add-exports jdk.hotspot.agent/sun.jvm.hotspot.oops=ALL-UNNAMED --add-exports jdk.hotspot.agent/sun.jvm.hotspot.memory=ALL-UNNAMED --add-exports jdk.hotspot.agent/sun.jvm.hotspot.gc.g1=ALL-UNNAMED --add-exports jdk.hotspot.agent/sun.jvm.hotspot.utilities=ALL-UNNAMED -jar heap-scanner-1.0-SNAPSHOT.jar <java proccess pid>
+```
---
+++ src/main/java/cf/mgorbunov/OldRegionHeapScanner.java
@@ -0,0 +1,120 @@
+package cf.mgorbunov;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import sun.jvm.hotspot.debugger.Address;
+import sun.jvm.hotspot.gc.g1.G1CollectedHeap;
+import sun.jvm.hotspot.gc.g1.HeapRegion;
+import sun.jvm.hotspot.gc.shared.CollectedHeap;
+import sun.jvm.hotspot.memory.Universe;
+import sun.jvm.hotspot.oops.Array;
+import sun.jvm.hotspot.oops.ByteField;
+import sun.jvm.hotspot.oops.FieldIdentifier;
+import sun.jvm.hotspot.oops.IndexableFieldIdentifier;
+import sun.jvm.hotspot.oops.ObjectHistogram;
+import sun.jvm.hotspot.oops.Oop;
+import sun.jvm.hotspot.oops.TypeArray;
+import sun.jvm.hotspot.oops.TypeArrayKlass;
+import sun.jvm.hotspot.runtime.BasicType;
+import sun.jvm.hotspot.runtime.VM;
+import sun.jvm.hotspot.tools.Tool;
+import sun.jvm.hotspot.utilities.AddressOps;
+
+public class OldRegionHeapScanner extends Tool {
+    private static boolean printByteArrays = false;
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            if ("--print-bytes-arrays".equals(arg)) {
+                printByteArrays = true;
+                break;
+            }
+        }
+        new OldRegionHeapScanner().execute(args);
+    }
+
+    @Override
+    public void run() {
+        try {
+            VM vm = VM.getVM();
+            Universe universe = vm.getUniverse();
+            CollectedHeap heap = universe.heap();
+
+            if (heap instanceof G1CollectedHeap) {
+                G1CollectedHeap g1CollectedHeap = (G1CollectedHeap) heap;
+                List<HeapRegionBounds> oldGenRegions = new ArrayList<>();
+                List<HeapRegionBounds> humongousGenRegions = new ArrayList<>();
+                Iterator<HeapRegion> heapRegionIterator = g1CollectedHeap.hrm().heapRegionIterator();
+                while (heapRegionIterator.hasNext()) {
+                    HeapRegion next = heapRegionIterator.next();
+                    HeapRegionBounds heapRegionBounds = new HeapRegionBounds(next.bottom(), next.end());
+                    if (next.isOld()) {
+                        oldGenRegions.add(heapRegionBounds);
+                    } else if (next.isHumongous()) {
+                        humongousGenRegions.add(heapRegionBounds);
+                    }
+                }
+
+                ObjectHistogram objectHistogram = new ObjectHistogram() {
+                    @Override
+                    public boolean doObj(Oop obj) {
+                        for (HeapRegionBounds region : oldGenRegions) {
+                            if (containsInRegion(obj, region)) {
+                                handleTypeArray(obj, vm);
+                                super.doObj(obj);
+                                return true;
+                            }
+                        }
+                        for (HeapRegionBounds region : humongousGenRegions) {
+                            if (containsInRegion(obj, region)) {
+                                handleTypeArray(obj, vm);
+                                super.doObj(obj);
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+                };
+                vm.getObjectHeap().iterate(objectHistogram);
+                objectHistogram.print();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleTypeArray(Oop obj, VM vm) {
+        if (printByteArrays) {
+            if (obj instanceof TypeArray) {
+                TypeArray typeArray = (TypeArray) obj;
+                long length = typeArray.getLength();
+                TypeArrayKlass typeArrayKlass = (TypeArrayKlass) typeArray.getKlass();
+                if (TypeArrayKlass.T_BYTE == typeArrayKlass.getElementType()) {
+                    byte[] arr = new byte[(int) length];
+                    for (int index = 0; index < length; index++) {
+                        FieldIdentifier id = new IndexableFieldIdentifier(index);
+                        long offset = Array.baseOffsetInBytes(BasicType.T_BYTE) + index * vm.getObjectHeap().getByteSize();
+                        ByteField byteField = new ByteField(id, offset, false);
+                        arr[index] = byteField.getValue(obj);
+                    }
+                    System.out.println(new String(arr));
+                }
+            }
+        }
+    }
+
+    private static boolean containsInRegion(Oop obj, HeapRegionBounds region) {
+        return AddressOps.gte(obj.getHandle(), region.bottom) && AddressOps.lt(obj.getHandle(), region.end);
+    }
+
+    static class HeapRegionBounds {
+        Address bottom;
+        Address end;
+
+        public HeapRegionBounds(Address bottom, Address end) {
+            this.bottom = bottom;
+            this.end = end;
+        }
+    }
+}
